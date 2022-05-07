942e9981e63ad881ff3950352ee94274f4df860e
Huilin Zhang <orangeade93626@gmail.com>
2022-05-06 23:43:40 -0700

    Update udp_ping_HuilinZhang_917071562_TingweiLiu_917707784.py

--- udp_ping_HuilinZhang_917071562_TingweiLiu_917707784.py
+++ udp_ping_HuilinZhang_917071562_TingweiLiu_917707784.py
@@ -17,35 +17,42 @@ def client_send():
     message = 'ping'
 
     clientSocket.sendto(message.encode(),(serverName, serverPort))
-    print("here")
-    modifiedMessage, serverAddress = clientSocket.recvfrom(4096)
+    # print("here")
+    # modifiedMessage, serverAddress = clientSocket.recvfrom(4096)
     # print(modifiedMessage)
     
     for i in range(0,10):
         now = time.time()
-        # while(time.time() - now<10):
-        #     pass
         timeout_count = 0
 
-        clientSocket.sendto(message.encode(),(serverName, serverPort))
-        # print("here")
-        modifiedMessage, serverAddress = clientSocket.recvfrom(4096)
+        try:
+            clientSocket.sendto(message.encode(),(serverName, serverPort))
+            modifiedMessage, serverAddress = clientSocket.recvfrom(4096)
+            if(modifiedMessage.decode() == 'PING'):
+                succeed_count += 1
+
+        except Exception as ex:
+            print("error: ",ex)
+            if (i != 0):
+                timeout_seconds = timeout_seconds * math.pow(2,timeout_count)+random.uniform(0,1)
+            timeout_count = timeout_count + 1
+            if (timeout_seconds >= 600):
+                timeout_seconds = 600
+            time.sleep(timeout_seconds)
+            continue
+
         # print(modifiedMessage)
         # 丢包
-        while(modifiedMessage.decode() != 'PING'):
-            print("not pint!")
-            if(time.time()-now >= timeout_seconds):
-                print("time out!")
-                inner_timeout = inner_timeout * math.pow(2,timeout_count)+random.uniform(0,1)
-                inner_now = time.time()
-                while(time.time()-inner_now <= inner_timeout):
-                    pass
-                timeout_count += 1
-            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-            
-        if(modifiedMessage.decode() == 'PING'):
-            succeed_count += 1
-        
+        # while(modifiedMessage.decode() != 'PING'):
+        #     print("not pint!")
+        #     if(time.time()-now >= timeout_seconds):
+        #         print("time out!")
+        #         inner_timeout = inner_timeout * math.pow(2,timeout_count)+random.uniform(0,1)
+        #         inner_now = time.time()
+        #         while(time.time()-inner_now <= inner_timeout):
+        #             pass
+        #         timeout_count += 1
+        #     modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
         print("The current time is: {} and this is message number: {}".format(now, (i+1)))
         print("Uppercase Message from the Server: {}".format(modifiedMessage))
