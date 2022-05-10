8806a5efe313fe1b9a0a52d0ef18f9df09e117dd
Huilin Zhang <97477503+Orangeadee@users.noreply.github.com>
2022-05-09 17:51:36 -0700

    add comments

--- udp_iperf_HuilinZhang_917071562_TingweiLiu_917707784.py
+++ udp_iperf_HuilinZhang_917071562_TingweiLiu_917707784.py
@@ -6,41 +6,58 @@ import random
 import os
 
 def client_send():
+    # Given server Name and Port Number.
     serverName = '173.230.149.18'
     serverPort = 5006
 
+    # Connect socket with udp protocal.
     clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+    # Message to send is ping.
+    # Path is where data are stored.
+    # TotalSize is the total size to be stored.
     message = 'ping'
     path = './data.txt'
     totalSize = 1300000
 
+    # Open the data file with permission 'a' to prevent overwrite.
     data_file = open(path, 'a')
+    # Record the size of the file.
     size = os.path.getsize(path)
+    # For storing further calculation of how many bytes written in the file.
     percentage = 0
+    # Start sending message to the server, only sending one time.
     clientSocket.sendto(message.encode(),(serverName, serverPort))
+    # Set timeout to 30 seconds: stop the program if can't receive the message in 30 seconds.
+    # The prompt said to set timeout to 10 seconds, but it will timeout before the data file
+    # is written 100%, it needs more time to receive data from the server. 
+    # Hence, we decide to set timeout to 30 seconds.
     clientSocket.settimeout(30)
     now = time.time()
     while(size <= totalSize):
+        # Receive message from the server and write the message to the data file.
         modifiedMessage, serverAddress = clientSocket.recvfrom(4096)
-
         data_file.write(modifiedMessage.decode())
         data_file.flush()
         os.fsync(data_file.fileno())
-
         
+        # Calculating the percentage of data file written / total size to be written.
         percentage = os.path.getsize(path) / totalSize * 100
+        # Print the result.
         print("received %: ", int(percentage))
+        # Update the size of data file.
         size = os.path.getsize(path)
+
+    # ========================= Print Final Result ========================= #
+    # Get the total time spent when writing to the data file.
     now = time.time() - now
     print("break")
     print("Time elapsed: {} seconds".format(now))
+    # Thoroughput = size of the file / total time
     throu = size / now
     print("Throughput: {} bps".format(throu))
     print("Size of file is {} bites".format(size))
     print("File Downloaded")
-
-
 
 def main():
     client_send()
--- udp_ping_HuilinZhang_917071562_TingweiLiu_917707784.py
+++ udp_ping_HuilinZhang_917071562_TingweiLiu_917707784.py
@@ -2,64 +2,70 @@ from socket import *
 import time
 import math
 import random
-
 import statistics
 
 def client_send():
+    # Given server Name and Port Number.
     serverName = '173.230.149.18'
     serverPort = 12000
+    # Set timeout seconds to 10s.
     timeout_seconds = 10.0
+    # For storing Round Trip Times.
     rtt_list = []
+    # Count for number of succeed packages.
     succeed_count = 0
-
+    # Connect socket with udp protocal.
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     message = 'ping'
         
+    # Sending 10 messages in total.
     for i in range(0,10):
+        # Start counting the time when sending message to the server.
         now = time.time()
+        # Timeout_count is for further calculation of timeout_seconds.
         timeout_count = 0
         try:
+            # Start sending message to server.
             clientSocket.sendto(message.encode(),(serverName, serverPort))
+            # Receive message from server with max buffer size of 4096.
             modifiedMessage, serverAddress = clientSocket.recvfrom(4096)
+            # If the decoded message is PING, then we successfully sending one message.
             if(modifiedMessage.decode() == 'PING'):
                 succeed_count += 1
-
+        # Throwing an exception meands the message didn't send successfully.
         except Exception as ex:
-            print("error: ",ex)
-            if (i != 0):
+            # Succeed_count increments by 1 every time we receive PING, so if the message
+            # failed to send, the count won't increment and will be the same as i.
+            # This will be its first timeout.
+            if (succeed_count == i):
+                # Apply the formula of timeout_seconds if it's not the first time it timeout
                 timeout_seconds = timeout_seconds * math.pow(2,timeout_count)+random.uniform(0,1)
+            # Increment count for timeout by 1.
             timeout_count = timeout_count + 1
+
+            # We need to limit the timeout seconds to less or equal to 10 minutes.
+            # 10 min = 10 * 60 = 600 seconds.
             if (timeout_seconds >= 600):
                 timeout_seconds = 600
             time.sleep(timeout_seconds)
+            # After sleeping for timeout_seconds amout of time, continue to next iteration.
             continue
-
-        # print(modifiedMessage)
-        # 丢包
-        # while(modifiedMessage.decode() != 'PING'):
-        #     print("not pint!")
-        #     if(time.time()-now >= timeout_seconds):
-        #         print("time out!")
-        #         inner_timeout = inner_timeout * math.pow(2,timeout_count)+random.uniform(0,1)
-        #         inner_now = time.time()
-        #         while(time.time()-inner_now <= inner_timeout):
-        #             pass
-        #         timeout_count += 1
-        #     modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-
+        # ========================= Print Result In Loop ========================= #
         print("The current time is: {} and this is message number: {}".format(now, (i+1)))
         print("Uppercase Message from the Server: {}".format(modifiedMessage))
+        # Calculate the current round trip time and store it in the list.
         rtt_time = time.time() - now
         rtt_list.append(rtt_time)
         print("The Round Trip Time is: {}".format(rtt_time))
 
+    # ========================= Print Final Result ========================= #
     print("the program is done")
     print("Stored RTTs are: ", rtt_list)
     print("Max RTT is: {}".format(max(rtt_list)))
     print("Min RTT is: {}".format(min(rtt_list)))
     print("Sum of all RTTs is: {}".format(sum(rtt_list)))
     print("Average Round Trip Time is: {}".format(statistics.mean(rtt_list)))
-
+    # Counts for package lost.
     lost_count = 10 - succeed_count
     print("Total number of packet lost is: ",lost_count)
     clientSocket.close()
